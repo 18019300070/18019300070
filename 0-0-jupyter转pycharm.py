96ada8b309a4acb0d3a7aa486b80152e7442d5bf
18019300070 <151984324+18019300070@users.noreply.github.com>
2024-02-14 11:21:15 +0800

    Add files via upload

---
+++ 0-0-jupyter转pycharm.py
@@ -0,0 +1,9 @@
+import os
+
+# 更改当前工作目录
+os.chdir("D:\\1\\Pythonproject\\刘经纬")
+
+os.system('jupyter nbconvert --to script Case016_基于三种超参数优化算法的客户购买保险可能性分析_代码.ipynb')
+
+
+# jupyter nbconvert --to script 7_共享单车-K-Means和GMM聚类算法.ipynb
